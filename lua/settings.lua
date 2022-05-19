350d1a436fabd40ac5696cd3d2e56fac1b049e36
Michael Cooper <mcooper628@gmail.com>
2022-05-18 23:26:23 -0700

    Create settings.lua

---
+++ lua/settings.lua
@@ -0,0 +1,17 @@
+local o = vim.o
+local opt = vim.opt
+
+o.relativenumber = true
+o.number = true
+opt.cc = { 90 }
+o.hlsearch = true
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = true
+o.shiftwidth = 4
+o.autoindent = 4
+opt.scrolloff = 24
+o.ttyfast = true
+opt.mouse = 'a'
+vim.cmd 'set guicursor=i:block'
+o.guitablabel = '%t'
